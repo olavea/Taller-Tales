94958785de76357ecc94fa1bb7ffe0b81a489779
olavea <ola@olavea.com>
2025-04-05 14:53:49 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,40 @@
+<x-layout>
+    <section>
+       <h3>Top Tales</h3>
+
+       <div class="p-4 bg-orange-500">
+        <div>
+            <div>Laracasts</div>
+            <div>
+                <h3>Vid pro</h3>
+                <p>full </p>
+            </div>
+            <div>
+                <div>
+                    <a href="#">Tag</a>
+                    <a href="#">Tag</a>
+                    <a href="#">Tag</a>
+                </div>
+            </div>
+            <div class="bg-red-100">
+            
+                <img src="http://place-hold.it/42/42" alt="">
+            </div>    
+            <div>
+            </div>
+        </div>
+       </div>   
+    </section>    
+    <div class="p-10">
+        He World!
+    </div>
+    <div>
+      Hel World!
+    </div>
+    <div>
+        Hell World!
+    </div>
+    <div>
+        Hello World!
+    </div>
+</x-layout>    
