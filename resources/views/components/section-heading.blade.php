a67b9804aa9d4096a4d8a1f5aa68460ef4d5a489
olavea <ola@olavea.com>
2025-04-06 13:18:06 +0200

    From Design to Blade

---
+++ resources/views/components/section-heading.blade.php
@@ -0,0 +1,7 @@
+<div class="inline-flex items-center gap-x-2">
+    <span class="w-2 h-2 bg-red-500 inline-block"></span> 
+    <h3 class="font-bold text-lg">
+        
+        {{ $slot }}
+    </h3>
+</div>
